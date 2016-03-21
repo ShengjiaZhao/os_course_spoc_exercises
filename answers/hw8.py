c712a818e02f59dbd6d33c60afa5784b57ceb1f3
Shengjia <zhaosj12@mails.tsinghua.edu.cn>
2016-03-21 09:27:24 +0800

    HW8

--- answers/hw8.py
+++ answers/hw8.py
@@ -16,6 +16,7 @@ class WorkingSet:
         while self.clock - self.history[0][1] >= self.window_size:
             del self.history[0]
 
+        print("Access " + str(index) + ":"),
         for item in self.history:
             print(item[0]),
         print("")
@@ -23,17 +24,50 @@ class WorkingSet:
 
 
 class MissRate:
-    def __init__(self, rate):
-        pass
+    def __init__(self, target_rate):
+        self.target_rate = target_rate
+        self.previous_miss = 0
+        self.memory = []
+        self.clock = 0
+        self.history = []
 
     def access(self, index):
-        pass
+        self.history.append(index)
+        if len(self.history) > self.target_rate + 1:
+            del self.history[0]
+        self.clock += 1
+
+        if index not in self.memory:
+            miss_interval = self.clock - self.previous_miss
+            self.previous_miss = self.clock
+
+            if miss_interval <= self.target_rate:
+                self.memory.append(index)
+            else:
+                new_memory = []
+                for item in self.memory:
+                    if item in self.history:
+                        new_memory.append(item)
+                self.memory = new_memory
+                self.memory.append(index)
+
+        print("Access " + str(index) + ":"),
+        for item in self.memory:
+            print(item),
+        print("")
 
 
 if __name__ == '__main__':
-    working_set = WorkingSet(4)
+
     sequence = ['e', 'd', 'a', 'c', 'c', 'd', 'b', 'c', 'e', 'c', 'e', 'a', 'd']
+
+    print("By working set algorithm:")
+    working_set = WorkingSet(4)
     for index in sequence:
         working_set.access(index)
 
+    print("By miss rate algorithm: ")
+    miss_rate = MissRate(2)
+    for index in sequence:
+        miss_rate.access(index)
 
